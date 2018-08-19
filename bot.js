2cf9f35a519022fb0b67b3abf16fe7a65cbd05b9
xBatuhan <42517642+xBatuhan@users.noreply.github.com>
2018-08-19 18:46:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,5 +107,4 @@ client.on('warn', e => {
 client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
-
-client.login(ayarlar.token);
+client.login(process.env.BOT_TOKEN);
